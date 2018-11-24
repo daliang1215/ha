84828ccb2ea7fad8f75ab300ed26a8dcc63a4170
Jimmy Chen <daliang1215@163.com>
2018-11-24 11:42:00 +0800

    增加了关闭音乐进程和自动语音识别的模块

--- script/play_sound.sh
+++ script/play_sound.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 date
-omxplayer -o local --vol -602 $1  
+if [ "$1"x = "jimmylin"x ] ; then # 林志颖
+	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/jimmylin.txt`
+elif [ "$1"x = "davewang"x ] ; then # 王杰
+	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/davewang.txt`
+elif [ "$1"x = "mayday"x ] ; then # 五月天
+	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/mayday.txt`
+elif [ "$1"x = "tingting"x ] ; then # 粤语歌曲
+	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/tingting.txt`
+else
+	txt=$1
+fi
+# qmi music
+omxplayer -o local --font /usr/share/fonts/truetype/wqy/wqy-zenhei.ttc  --vol -602  $txt 
 date
