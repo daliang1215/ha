9599eb0e632acf1fdef3912a194f4380f2c2246f
Jimmy Chen <daliang1215@163.com>
2018-12-05 12:26:24 +0800

    增加了周华健、张信哲、班得瑞歌曲识别

--- script/play_sound.sh
+++ script/play_sound.sh
@@ -8,6 +8,12 @@ elif [ "$1"x = "mayday"x ] ; then # 五月天
 	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/mayday.txt`
 elif [ "$1"x = "tingting"x ] ; then # 粤语歌曲
 	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/tingting.txt`
+elif [ "$1"x = "bandari"x ] ; then # 班得瑞/bandari
+	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/bandari.txt`
+elif [ "$1"x = "zhouhuajian"x ] ; then # 周华健
+	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/zhouhuajian.txt`
+elif [ "$1"x = "zhangxinzhe"x ] ; then # 张信哲
+	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/zhangxinzhe.txt`
 else
 	txt=$1
 fi
