e23153e75ce9dede8db2507ab8e164aabfaeea98
Jimmy Chen <daliang1215@163.com>
2019-01-02 08:42:12 +0800

    增加了网易云音乐和语音识别的适配，可以根据识别的歌曲自动播放网易云音乐歌曲，基于NeteaseCloudMusicApi

--- script/play_sound.sh
+++ script/play_sound.sh
@@ -14,8 +14,32 @@ elif [ "$1"x = "zhouhuajian"x ] ; then # 周华健
 	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/zhouhuajian.txt`
 elif [ "$1"x = "zhangxinzhe"x ] ; then # 张信哲
 	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/zhangxinzhe.txt`
-else
-	txt=$1
+elif [ "$1"x = "qmi"x ] ; then # Q米
+	txt=`shuf -n1 /mnt/disks/music/lossless无损/list/qmi.txt`
+elif [ "$1"x = "xiaoshoupaipai"x ] ; then # 小手拍拍
+	txt='/mnt/disks/music/qmi/小手拍拍.mp3'
+elif [ "$1"x = "hekuailezaiyiqi"x ] ; then # 和快乐在一起
+	txt='/mnt/disks/music/qmi/和快乐在一起.mp3'
+elif [ "$1"x = "huangliniao"x ] ; then # 蜗牛和黄鹂鸟
+	txt='/mnt/disks/music/qmi/蜗牛和黄鹂鸟.mp3'
+elif [ "$1"x = "shishangzhiyoumamahao"x ] ; then # 世上只有妈妈好
+	txt='/mnt/disks/music/qmi/世上只有妈妈好.mp3'
+elif [ "$1"x = "beishuang"x ] ; then # 倍爽
+	txt='/mnt/disks/music/qmi/倍爽.mp3'
+elif [ "$1"x = "jiuyuejiudejiu"x ] ; then # 九月九的酒
+	txt='/mnt/disks/music/qmi/九月九的酒.mp3'
+elif [ "$1"x = "xiaojingcha"x ] ; then # 我在马路边捡到一分钱
+	txt='/mnt/disks/music/qmi/我在马路边捡到一分钱.mp3'
+elif [ "$1"x = "xiaocangwa"x ] ; then # 小仓娃
+	txt='/mnt/disks/music/qmi/豫剧-小仓娃.mp3'
+else # 163 music
+	## get music from 163
+	## music from $1
+	wget -O /tmp/${1}.txt "http://192.168.88.140:3000/search?keywords=${1}&type=1&limit=1"  2>/dev/null 
+	#songid=`cat /tmp/${1}.txt |grep -Po '(?<=id":)[0-9]+'|head -n 1`
+	songid=`cat /tmp/${1}.txt |jq . |jq '.result.songs[].id'|head -n 1`
+	wget -O /tmp/${1}.mp3 "https://music.163.com/song/media/outer/url?id=${songid}.mp3"
+	txt="/tmp/${1}.mp3"
 fi
 # qmi music
 omxplayer -o local --font /usr/share/fonts/truetype/wqy/wqy-zenhei.ttc  --vol -602  $txt 
