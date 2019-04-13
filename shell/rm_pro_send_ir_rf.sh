3afed1305396ac1eb1cda64f49f7303385eb2fa3
Jimmy Chen <daliang1215@163.com>
2019-04-14 07:53:10 +0800

    适配天猫精灵，修改射频控制用自带的switch替换了原来写脚本的方式

--- shell/rm_pro_send_ir_rf.sh
+++ shell/rm_pro_send_ir_rf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cd /home/pi/.homeassistant/script/python-broadlink/cli
+cd /home/pi/.homeassistant/shell/python-broadlink/cli
+
 
 ./broadlink_cli --host 192.168.88.166 --mac 780F775A6CC2 --send $1
